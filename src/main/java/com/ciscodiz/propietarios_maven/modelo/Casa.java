58f75d8e4725a40890be94d0d8a1feb5d84999d1
noseq <noseq@DESKTOP-BVHR5IV>
2019-05-11 12:31:43 +0100

    clases basicas (falta de revision) v 0.0.2

--- src/main/java/com/ciscodiz/propietarios_maven/modelo/Casa.java
+++ src/main/java/com/ciscodiz/propietarios_maven/modelo/Casa.java
@@ -10,5 +10,89 @@ package com.ciscodiz.propietarios_maven.modelo;
  * @author noseq
  */
 public class Casa {
-    
+     public enum TipoVia{
+         CALLE
+         ,PLAZA
+         ,AV
+         ,CALLEJON
+     }
+     public enum TipoVivienda{
+         CASA
+         ,PISO
+     }
+     private static int idCasaStatic = 0;
+     private final int idCasaInstance;
+     private int metrosCuadrados;
+     private int numero;
+     private TipoVia tipoVia;
+     private TipoVivienda tipoVivienda;
+     private Integer planta;
+     private Integer piso;
+
+    public Casa(int metrosCuadrados, int numero, TipoVia tipoVia
+            , TipoVivienda tipoVivienda, Integer planta, Integer piso) {
+        idCasaInstance = idCasaStatic++;
+        this.metrosCuadrados = metrosCuadrados;
+        this.numero = numero;
+        this.tipoVia = tipoVia;
+        this.tipoVivienda = tipoVivienda;
+        this.planta = planta;
+        this.piso = piso;
+    }
+
+    public static int getIdCasaStatic() {
+        return idCasaStatic;
+    }
+
+    public static void setIdCasaStatic(int idCasaStatic) {
+        Casa.idCasaStatic = idCasaStatic;
+    }
+
+    public int getMetrosCuadrados() {
+        return metrosCuadrados;
+    }
+
+    public void setMetrosCuadrados(int metrosCuadrados) {
+        this.metrosCuadrados = metrosCuadrados;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public TipoVia getTipoVia() {
+        return tipoVia;
+    }
+
+    public void setTipoVia(TipoVia tipoVia) {
+        this.tipoVia = tipoVia;
+    }
+
+    public TipoVivienda getTipoVivienda() {
+        return tipoVivienda;
+    }
+
+    public void setTipoVivienda(TipoVivienda tipoVivienda) {
+        this.tipoVivienda = tipoVivienda;
+    }
+
+    public Integer getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(Integer planta) {
+        this.planta = planta;
+    }
+
+    public Integer getPiso() {
+        return piso;
+    }
+
+    public void setPiso(Integer piso) {
+        this.piso = piso;
+    } 
 }
--- src/main/java/com/ciscodiz/propietarios_maven/modelo/Propietario.java
+++ src/main/java/com/ciscodiz/propietarios_maven/modelo/Propietario.java
@@ -5,14 +5,83 @@
  */
 package com.ciscodiz.propietarios_maven.modelo;
 
+import java.util.ArrayList;
+import javax.management.InvalidAttributeValueException;
+
 /**
  *
  * @author noseq
  */
 public class Propietario {
+
     private String nombre;
     private String apellido1;
     private String apellido2;
+    private Integer edad; //atributo nulable
     private DNI dni;
+    private ArrayList<Casa> casasLista;
+
+    public Propietario(String nombre, String apellido1, String apellido2,
+             char letra, int numero) {
+        try {
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            dni = new DNI(letra, numero);
+            casasLista = new ArrayList<>();
+        } catch (InvalidAttributeValueException e) {
+            System.out.println(e);
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    
+    
+    public DNI getDni() {
+        return dni;
+    }
+
+    public void setDni(DNI dni) {
+        this.dni = dni;
+    }
+
+    public ArrayList<Casa> getCasasLista() {
+        return casasLista;
+    }
+
+    public void setCasasLista(ArrayList<Casa> casasLista) {
+        this.casasLista = casasLista;
+    }
     
 }
